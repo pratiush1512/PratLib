e7803093163dda3d34b7510a619a011944b5f0f8
Pratiush Anand <92681193+pratiush1512@users.noreply.github.com>
2025-03-22 00:09:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@
 from setuptools import setup, find_packages
 setup(
     name="pratlib",
-    version="1.0.4",
+    version="1.0.5",
     author="Pratiush",
     author_email="pratiushanand1@gmail.com",
     description="A PySpark library for machine learning, similar to scikit-learn.",
